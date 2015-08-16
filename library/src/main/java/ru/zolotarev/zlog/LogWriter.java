24c02efe2c55ecc8214e082f06f56feb27638db7
Zahar <zahar.zolotarev@gmail.com>
2015-08-16 14:45:49 +0300

    write more information about device

--- library/src/main/java/ru/zolotarev/zlog/LogWriter.java
+++ library/src/main/java/ru/zolotarev/zlog/LogWriter.java
@@ -1,12 +1,9 @@
 package ru.zolotarev.zlog;
 
 import android.content.Context;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
+import android.os.Build;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 
 public class LogWriter {
@@ -16,17 +13,84 @@ public class LogWriter {
 	}
 
 	static void write(Context context,String msg,Throwable exception){
+		String logFile = SessionManager.getFilePath(context);
+		File file = new File(logFile);
+		StringBuilder builder = new StringBuilder();
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+				getDeviceInfo(builder);
+				builder.append("\n");
+			} catch (IOException e) {
+				return;
+			}
+		}
+		builder.append("\n");
+		builder.append(msg);
+		builder.append("\n");
+
+		if (exception != null) {
+			FileUtils.processThrowable(exception, builder);
+		}
+
+		FileUtils.print(file,builder.toString());
 	}
 
+	static void getDeviceInfo(StringBuilder builder){
+		builder.append("App version ")
+		       .append(BuildConfig.VERSION_NAME)
+		       .append(" (")
+		       .append(BuildConfig.VERSION_CODE)
+		       .append(")")
+		       .append("\nDebug-infos:")
+		       .append("\n OS Version: ")
+		       .append(System.getProperty("os.version"))
+		       .append("(")
+		       .append(Build.VERSION.INCREMENTAL)
+		       .append(")")
+		       .append("\n OS API Level: ")
+		       .append(Build.VERSION.SDK_INT)
+		       .append("\n Device: ")
+		       .append(Build.DEVICE)
+		       .append("\n Model (and Product): ")
+		       .append(Build.MODEL)
+		       .append(" (")
+		       .append(Build.PRODUCT)
+		       .append(")")
+		       .append("\n RELEASE: ")
+		       .append(Build.VERSION.RELEASE)
+		       .append("\n BRAND: ")
+		       .append(Build.BRAND)
+		       .append("\n DISPLAY: ")
+		       .append(Build.DISPLAY);
 
-
-	private static String checkFirstLog(Context context){
-		try {
-			PackageInfo pack = context.getPackageManager().getPackageInfo(context.getPackageName(), PackageManager.GET_ACTIVITIES);
-			return "App version "+pack.versionName + " ("+pack.versionCode+")";
-		} catch (PackageManager.NameNotFoundException e) {
-			e.printStackTrace();
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+			builder.append("\n CPU_ABIS: ")
+			       .append(Build.SUPPORTED_ABIS)
+			       .append("\n CPU_ABIS_32: ")
+			       .append(Build.SUPPORTED_32_BIT_ABIS)
+			       .append("\n CPU_ABIS_64: ")
+			       .append(Build.SUPPORTED_64_BIT_ABIS);
+		}else{
+			builder.append("\n CPU_ABI: ")
+			       .append(Build.CPU_ABI)
+			       .append("\n CPU_ABI2: ")
+			       .append(Build.CPU_ABI2);
 		}
-		return null;
+
+		builder.append("\n UNKNOWN: ")
+		       .append(android.os.Build.UNKNOWN)
+		       .append("\n HARDWARE: ")
+		       .append(android.os.Build.HARDWARE)
+		       .append("\n Build ID: ")
+		       .append(android.os.Build.ID)
+		       .append("\n MANUFACTURER: ")
+		       .append(android.os.Build.MANUFACTURER)
+		       .append("\n SERIAL: ")
+		       .append(android.os.Build.SERIAL)
+		       .append("\n USER: ")
+		       .append(android.os.Build.USER)
+		       .append("\n HOST: ")
+		       .append(android.os.Build.HOST);
 	}
 }
