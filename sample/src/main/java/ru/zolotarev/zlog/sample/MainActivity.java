9921e87efe782f8d8c34e8b29c304716c4af95d8
Zahar <zahar.zolotarev@gmail.com>
2015-08-16 16:31:35 +0300

    Exclude appcompat lib

--- sample/src/main/java/ru/zolotarev/zlog/sample/MainActivity.java
+++ sample/src/main/java/ru/zolotarev/zlog/sample/MainActivity.java
@@ -1,11 +1,11 @@
 package ru.zolotarev.zlog.sample;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 
 import ru.zolotarev.zlog.ZLog;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
